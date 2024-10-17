09aa4c9a6d4b3991dfb469e0afc2f35f07511ebc
JaimeRodGon <jaimerodriguezgonzalez94@gmail.com>
2024-10-17 22:58:40 +0200

    Prueba de un push por Jaime

--- app/src/main/java/com/android/virodevdb/AuthActivity.java
+++ app/src/main/java/com/android/virodevdb/AuthActivity.java
@@ -22,6 +22,7 @@ import java.security.KeyStore;
 public class AuthActivity extends AppCompatActivity {
 
     //objetos
+    //Prueba de un push 22:56 por Jaime
     private Button botonRegistrar;
     private Button botonAcceder;
     private TextView textoEmail;
